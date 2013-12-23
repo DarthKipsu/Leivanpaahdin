156098965ccf4ce750ff3f80c18298fe41874f23
Verna Koskinen <darth.kipsu@gmail.com>
2013-12-23 20:58:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leivanpaahdin::Application.config.secret_key_base = 'd42793e4a9acac9d4fc768efac9598955dd70780c7ed03d809ac0bd2b54f192b40b6ac1d31a69e59218d5ed922e80ee692166ed9fcea80b2a19085d66e78114e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Leivanpaahdin::Application.config.session_store :cookie_store, key: '_leivanpaahdin_session'
