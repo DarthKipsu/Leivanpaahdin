413fae63437a5abfbdcc1aee3994301cdf73ee77
Verna Koskinen <darth.kipsu@gmail.com>
2013-12-26 13:45:13 +0200

    get bulb information

--- app/models/hue.rb
+++ app/models/hue.rb
@@ -1,23 +1,50 @@
 class Hue
 
+	# Get groups and bulbs
 	def groups
 		groups = {
-			'group1' => Huegroup.new('Bedroom', 1),
-			'group2' => Huegroup.new('Study', 3)
+			'group1' => Huegroup.new('Bedroom', bedroom_bulbs),
+			'group2' => Huegroup.new('Study', study_bulbs)
 		}		
+	end
+
+	# Select bulbs that belong in bedroom
+	def bedroom_bulbs
+		bedroom_bulbs = [
+			Huebulb.new(1)
+		]
+	end
+
+	# Select bulbs that belong in study
+	def study_bulbs
+		study_bulbs = [
+			Huebulb.new(2),
+			Huebulb.new(3)
+		]
 	end
 
 end
 
 class Huegroup
 
+	attr_accessor :name, :bulbs
+
+	# Give group names to Hue class
 	def initialize(name, bulbs)
 		@name = name
 		@bulbs = bulbs
 	end
 
-	def name
-		@name		
+
+end
+
+class Huebulb
+
+	attr_accessor :id
+
+	# Give bulb id to Hue class
+	def initialize(id)
+		@id = id
 	end
 
 end
