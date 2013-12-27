22177e0d459cf7400236c803640b80da3b8233d7
Verna Koskinen <darth.kipsu@gmail.com>
2013-12-27 16:33:46 +0200

    add new bulbs and group

--- app/models/hue.rb
+++ app/models/hue.rb
@@ -1,10 +1,11 @@
 class Hue
 
-	# Get groups and bulbs
+	# Get groups and bulbs in differnet groups
 	def groups
 		groups = {
 			'group1' => Huegroup.new('Bedroom', bedroom_bulbs),
-			'group2' => Huegroup.new('Study', study_bulbs)
+			'group2' => Huegroup.new('Hall', hall_bulbs),
+			'group3' => Huegroup.new('Study', study_bulbs)
 		}		
 	end
 
@@ -15,11 +16,20 @@ class Hue
 		]
 	end
 
+	# Select bulbs that belong in hall
+	def hall_bulbs
+		hall_bulbs = [
+			Huebulb.new(5),
+			Huebulb.new(6)
+		]
+	end
+
 	# Select bulbs that belong in study
 	def study_bulbs
 		study_bulbs = [
 			Huebulb.new(2),
-			Huebulb.new(3)
+			Huebulb.new(3),
+			Huebulb.new(4)
 		]
 	end
 
@@ -29,7 +39,7 @@ class Huegroup
 
 	attr_accessor :name, :bulbs
 
-	# Give group names to Hue class
+	# Give group names and bulb methods to Hue class
 	def initialize(name, bulbs)
 		@name = name
 		@bulbs = bulbs
--- app/models/huetheme.rb
+++ app/models/huetheme.rb
@@ -1,10 +1,12 @@
 class Huetheme
 
 	# Get groups and assign themes
+	# 1=bedroom, 2=hall, 3=study (same as hue.rb)
 	def group_themes
 		group_themes = {
 			'group1' => Grouptheme.new(group1_themes),
-			'group2' => Grouptheme.new(group2_themes)
+			'group2' => Grouptheme.new(group2_themes),
+			'group3' => Grouptheme.new(group3_themes)
 		}		
 	end
 
@@ -25,13 +27,21 @@ class Huetheme
 		]
 	end
 
+	# Themes for group3
+	def group3_themes
+		group3_themes = [
+			Themename.new('Theme31'),
+			Themename.new('Theme32')
+		]
+	end
+
 end
 
 class Grouptheme
 
 	attr_accessor :theme_name
 
-	# Give theme name to group themes
+	# get different themes fro each room
 	def initialize(theme_name)
 		@theme_name = theme_name
 	end
