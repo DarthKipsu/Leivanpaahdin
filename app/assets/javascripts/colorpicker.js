35bd0e4aae261e1362e95e327496c23b3362b39f
Verna Koskinen <darth.kipsu@gmail.com>
2014-01-08 10:40:39 +0200

    cleans colorpicker.js

--- app/assets/javascripts/colorpicker.js
+++ app/assets/javascripts/colorpicker.js
@@ -8,25 +8,7 @@
     var type = (window.SVGAngle || document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1") ? "SVG" : "VML"),
         picker, slide, hueOffset = 15, svgNS = 'http://www.w3.org/2000/svg';
 
-    // This HTML snippet is inserted into the innerHTML property of the passed color picker element
-    // when the no-hassle call to ColorPicker() is used, i.e. ColorPicker(function(hex, hsv, rgb) { ... });
-    
-    var colorpickerHTMLSnippet = [
-        
-        '<div class="slide-wrapper">',
-                '<div class="slide"></div>',
-                '<div class="slide-indicator"></div>',
-        '</div>',
-        '<div class="picker-wrapper">',
-                '<div class="picker"></div>',
-                '<div class="picker-indicator"></div>',
-        '</div>'
-        
-    ].join('');
-
-    /**
-     * Return mouse position relative to the element el.
-     */
+    // Return mouse position relative to the element el.
     function mousePosition(evt) {
         // IE:
         if (window.event && window.event.contentOverflow !== undefined) {
@@ -41,23 +23,20 @@
         return { x: evt.layerX - wrapper.offsetLeft, y: evt.layerY - wrapper.offsetTop };
     }
 
-    /**
-     * Create SVG element.
-     */
+    // Create SVG element.
     function $(el, attrs, children) {
         el = document.createElementNS(svgNS, el);
         for (var key in attrs)
             el.setAttribute(key, attrs[key]);
         if (Object.prototype.toString.call(children) != '[object Array]') children = [children];
-        var i = 0, len = (children[0] && children.length) || 0;
+        var i = 0,
+            len = (children[0] && children.length) || 0;
         for (; i < len; i++)
             el.appendChild(children[i]);
         return el;
     }
 
-    /**
-     * Create slide and picker markup depending on the supported technology.
-     */
+    // Create slide and picker markup depending on the supported technology.
     if (type == 'SVG') {
 
         slide = $('svg', { xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: '100%', height: '100%' },
@@ -128,10 +107,8 @@
             document.namespaces.add('v', 'urn:schemas-microsoft-com:vml', '#default#VML');
     }
 
-    /**
-     * Convert HSV representation to RGB HEX string.
-     * Credits to http://www.raphaeljs.com
-     */
+    // Convert HSV representation to RGB HEX string.
+    // Credits to http://www.raphaeljs.com
     function hsv2rgb(hsv) {
         var R, G, B, X, C;
         var h = (hsv.h % 360) / 60;
@@ -151,11 +128,9 @@
         return { r: r, g: g, b: b, hex: "#" + (16777216 | b | (g << 8) | (r << 16)).toString(16).slice(1) };
     }
 
-    /**
-     * Convert RGB representation to HSV.
-     * r, g, b can be either in <0,1> range or <0,255> range.
-     * Credits to http://www.raphaeljs.com
-     */
+    // Convert RGB representation to HSV.
+    // r, g, b can be either in <0,1> range or <0,255> range.
+    // Credits to http://www.raphaeljs.com
     function rgb2hsv(rgb) {
 
         var r = rgb.r;
@@ -180,10 +155,8 @@
         return { h: H, s: S, v: V };
     }
 
-    /**
-     * Return click event handler for the slider.
-     * Sets picker background color and calls ctx.callback if provided.
-     */  
+    // Return click event handler for the slider.
+    // Sets picker background color and calls ctx.callback if provided.
     function slideListener(ctx, slideElement, pickerElement) {
         return function(evt) {
             evt = evt || window.event;
@@ -196,10 +169,8 @@
         }
     };
 
-    /**
-     * Return click event handler for the picker.
-     * Calls ctx.callback if provided.
-     */  
+    // Return click event handler for the picker.
+    // Calls ctx.callback if provided.
     function pickerListener(ctx, pickerElement) {
         return function(evt) {
             evt = evt || window.event;
@@ -216,12 +187,10 @@
 
     var uniqID = 0;
     
-    /**
-     * ColorPicker.
-     * @param {DOMElement} slideElement HSV slide element.
-     * @param {DOMElement} pickerElement HSV picker element.
-     * @param {Function} callback Called whenever the color is changed provided chosen color in RGB HEX format as the only argument.
-     */
+    // ColorPicker.
+    // @param {DOMElement} slideElement HSV slide element.
+    // @param {DOMElement} pickerElement HSV picker element.
+    // @param {Function} callback Called whenever the color is changed provided chosen color in RGB HEX format as the only argument.
     function ColorPicker(slideElement, pickerElement, callback) {
         
         if (!(this instanceof ColorPicker)) return new ColorPicker(slideElement, pickerElement, callback);
@@ -229,33 +198,10 @@
         this.h = 0;
         this.s = 1;
         this.v = 1;
-
-        if (!callback) {
-            // call of the form ColorPicker(element, funtion(hex, hsv, rgb) { ... }), i.e. the no-hassle call.
-
-            var element = slideElement;
-            element.innerHTML = colorpickerHTMLSnippet;
-            
-            this.slideElement = element.getElementsByClassName('slide')[0];
-            this.pickerElement = element.getElementsByClassName('picker')[0];
-            var slideIndicator = element.getElementsByClassName('slide-indicator')[0];
-            var pickerIndicator = element.getElementsByClassName('picker-indicator')[0];
-            
-            ColorPicker.fixIndicators(slideIndicator, pickerIndicator);
-
-            this.callback = function(hex, hsv, rgb, pickerCoordinate, slideCoordinate) {
-
-                ColorPicker.positionIndicators(slideIndicator, pickerIndicator, slideCoordinate, pickerCoordinate);
-                
-                pickerElement(hex, hsv, rgb);
-            };
-            
-        } else {
         
-            this.callback = callback;
-            this.pickerElement = pickerElement;
-            this.slideElement = slideElement;
-        }
+        this.callback = callback;
+        this.pickerElement = pickerElement;
+        this.slideElement = slideElement;
 
         if (type == 'SVG') {
 
@@ -311,12 +257,11 @@
         }
     }
 
-   /**
-    * Enable drag&drop color selection.
-    * @param {object} ctx ColorPicker instance.
-    * @param {DOMElement} element HSV slide element or HSV picker element.
-    * @param {Function} listener Function that will be called whenever mouse is dragged over the element with event object as argument.
-    */
+    // Enable drag&drop color selection.
+    // @param {object} ctx ColorPicker instance.
+    // @param {DOMElement} element HSV slide element or HSV picker element.
+    // @param {Function} listener Function that will be called whenever mouse is dragged over the element with event object as argument.
+
     function enableDragging(ctx, element, listener) {
         
         var mousedown = false;
@@ -327,7 +272,7 @@
         addEventListener(element, 'mousemove', function(evt) {
 
             if (mousedown) {
-                
+
                 listener(evt);
             }
         });
@@ -358,13 +303,11 @@
         return { r: parseInt(hex.substr(1, 2), 16), g: parseInt(hex.substr(3, 2), 16), b: parseInt(hex.substr(5, 2), 16) };
     };
 
-    /**
-     * Sets color of the picker in hsv/rgb/hex format.
-     * @param {object} ctx ColorPicker instance.
-     * @param {object} hsv Object of the form: { h: <hue>, s: <saturation>, v: <value> }.
-     * @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
-     * @param {string} hex String of the form: #RRGGBB.
-     */
+    // Sets color of the picker in hsv/rgb/hex format.
+    // @param {object} ctx ColorPicker instance.
+    // @param {object} hsv Object of the form: { h: <hue>, s: <saturation>, v: <value> }.
+    // @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
+    // @param {string} hex String of the form: #RRGGBB.
      function setColor(ctx, hsv, rgb, hex) {
          ctx.h = hsv.h % 360;
          ctx.s = hsv.s;
@@ -390,37 +333,29 @@
          return ctx;
     };
 
-    /**
-     * Sets color of the picker in rgb format.
-     * @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
-     */
+    // Sets color of the picker in hsv format.
+    // @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
     ColorPicker.prototype.setHsv = function(hsv) {
         return setColor(this, hsv);
     };
     
-    /**
-     * Sets color of the picker in rgb format.
-     * @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
-     */
+    // Sets color of the picker in rgb format.
+    // @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
     ColorPicker.prototype.setRgb = function(rgb) {
         return setColor(this, rgb2hsv(rgb), rgb);
     };
 
-    /**
-     * Sets color of the picker in hex format.
-     * @param {string} hex Hex color format #RRGGBB.
-     */
+    // Sets color of the picker in hex format.
+    // @param {string} hex Hex color format #RRGGBB.
     ColorPicker.prototype.setHex = function(hex) {
         return setColor(this, ColorPicker.hex2hsv(hex), undefined, hex);
     };
 
-    /**
-     * Helper to position indicators.
-     * @param {HTMLElement} slideIndicator DOM element representing the indicator of the slide area.
-     * @param {HTMLElement} pickerIndicator DOM element representing the indicator of the picker area.
-     * @param {object} mouseSlide Coordinates of the mouse cursor in the slide area.
-     * @param {object} mousePicker Coordinates of the mouse cursor in the picker area.
-     */
+    // Helper to position indicators.
+    // @param {HTMLElement} slideIndicator DOM element representing the indicator of the slide area.
+    // @param {HTMLElement} pickerIndicator DOM element representing the indicator of the picker area.
+    // @param {object} mouseSlide Coordinates of the mouse cursor in the slide area.
+    // @param {object} mousePicker Coordinates of the mouse cursor in the picker area.
     ColorPicker.positionIndicators = function(slideIndicator, pickerIndicator, mouseSlide, mousePicker) {
         
         if (mouseSlide) {
@@ -432,9 +367,7 @@
         } 
     };
 
-    /**
-     * Helper to fix indicators - this is recommended (and needed) for dragable color selection (see enabledDragging()).
-     */
+    // Helper to fix indicators - this is recommended (and needed) for dragable color selection (see enabledDragging()).
     ColorPicker.fixIndicators = function(slideIndicator, pickerIndicator) {
 
         pickerIndicator.style.pointerEvents = 'none';
